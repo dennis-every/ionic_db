3426c90c54738b26b68fcc453c339949c47a9113
Dennis Every <dennis@ansyra.com>
2019-10-13 19:17:21 -0400

    Added db service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'developers', pathMatch: 'full' },
+  { path: 'developers', loadChildren: './pages/developers/developers.module#DevelopersPageModule' },
+  { path: 'developers/:id', loadChildren: './pages/developer/developer.module#DeveloperPageModule' },
 ];
 
 @NgModule({
